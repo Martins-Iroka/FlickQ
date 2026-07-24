6813f9024a3b44104dc79bd77f19970284247b7b
Ikechukwu Iroka <martins.iroka@gmail.com>
2026-07-24 19:19:08 +0100

    enable BackgroundJobs.kt

--- server/src/main/kotlin/com/martdev/flickq/plugins/BackgroundJobs.kt
+++ server/src/main/kotlin/com/martdev/flickq/plugins/BackgroundJobs.kt
@@ -1,6 +1,8 @@
 package com.martdev.flickq.plugins
 
 import com.martdev.flickq.features.auth.domain.service.UserService
+import com.martdev.flickq.features.payment.domain.service.PaymentService
+import com.martdev.flickq.features.reservation.domain.service.ReservationService
 import io.ktor.server.application.Application
 import io.ktor.server.application.ApplicationStopping
 import kotlinx.coroutines.Job
@@ -9,18 +11,19 @@ import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import org.koin.ktor.ext.inject
 import kotlin.time.Duration.Companion.hours
+import kotlin.time.Duration.Companion.minutes
 
 fun Application.configureBackgroundJobs() {
     val userService by inject<UserService>()
-    /*val reservationService by inject<ReservationService>()
-    val paymentService by inject<PaymentService>()*/
+    val reservationService by inject<ReservationService>()
+    val paymentService by inject<PaymentService>()
     launch {
         while (isActive) {
             delay(24.hours)
             userService.deleteExpiredRefreshToken()
         }
     }
-    /*launch {
+    launch {
         while (isActive) {
             delay(1.minutes)
             reservationService.cancelExpiredReservations()
@@ -31,7 +34,7 @@ fun Application.configureBackgroundJobs() {
             delay(5.minutes)
             paymentService.reconcilePendingPayments()
         }
-    }*/
+    }
     monitor.subscribe(ApplicationStopping) {
         coroutineContext[Job]?.cancel()
     }
