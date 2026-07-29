4ffa8ae9b5f18eff24f5f2ec8a23199732d9d1ae
Ikechukwu Iroka <martins.iroka@gmail.com>
2026-07-29 12:05:47 +0100

    cleaned up UrlOpener.android.kt

--- feature/payment/presentation/src/androidMain/kotlin/com/martdev/flickq/feature/payment/presentation/UrlOpener.android.kt
+++ feature/payment/presentation/src/androidMain/kotlin/com/martdev/flickq/feature/payment/presentation/UrlOpener.android.kt
@@ -1,8 +1,6 @@
 package com.martdev.flickq.feature.payment.presentation
 
-import android.content.Intent
 import androidx.activity.ComponentActivity
-import androidx.activity.result.ActivityResultLauncher
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.core.net.toUri
 import org.koin.core.module.Module
@@ -11,12 +9,10 @@ import org.koin.dsl.module
 
 
 class AndroidPaystackUrl : UrlOpener {
-    private var terminalStatusChecked = false
     private var awaitingRedirect: Boolean = false
     private var cancelCallback: (() -> Unit)? = null
     private var resultCallback : (() -> Unit)? = null
 
-    private var checkoutLauncher: ActivityResultLauncher<Intent>? = null
     private var activity: ComponentActivity? = null
 
     override fun launchCheckout(
@@ -25,37 +21,22 @@ class AndroidPaystackUrl : UrlOpener {
         onCancel: () -> Unit,
         onResult: () -> Unit
     ) {
-//        terminalStatusChecked = false
         awaitingRedirect = true
         cancelCallback = onCancel
         resultCallback = onResult
 
-        /*val customTabs = CustomTabsIntent.Builder().build()
-        val intent = customTabs.intent
-        intent.data = checkoutUrl.toUri()*/
         CustomTabsIntent.Builder().build().launchUrl(activity!!, checkoutUrl.toUri())
-
-//        checkoutLauncher?.launch(intent)
     }
 
     override fun handleRedirect() {
         println("HandleRedirect called")
         if (!awaitingRedirect) return
-//        terminalStatusChecked = true
         awaitingRedirect = false
         resultCallback?.invoke()
     }
 
     fun register(activity: ComponentActivity) {
         this.activity = activity
-
-        /*checkoutLauncher = activity.registerForActivityResult(
-            ActivityResultContracts.StartActivityForResult()
-        ) { r ->
-            if (!terminalStatusChecked) {
-                cancelCallback?.invoke()
-            }
-        }*/
     }
 
     fun unregister() {
